c212555d51a4a453a04e779d51316d07fbc45bd9
bLue <tbdblue@gmail.com>
2019-10-25 21:16:23 +0800

    U

---
+++ src/main/services/env-installer.ts
@@ -0,0 +1,64 @@
+import { isMac, isWindows } from "@/utils/platform";
+import { spawn } from "@/utils/child-process";
+
+export async function installXcodeCLT() {
+  try {
+    if (isMac) {
+      await spawn('[installXcodeCLT]', 'xcode-select', ['--install']);
+      return true;
+    }
+  } catch (e) { }
+  return false;
+}
+
+export async function installGccAndGdb() {
+  if (isMac) {
+    return await installXcodeCLT();
+  } else if (isWindows) {
+    // 解压 MinGW64
+    // 设置 PATH
+    return true;
+  }
+  return false;
+}
+
+export async function installPython() {
+  if (isMac) {
+    return true;
+  } else if (isWindows) {
+
+    return true;
+  }
+  return false;
+}
+
+export async function installCpplint() {
+  try {
+    // 解压 cpplint 项目代码包
+    // 安装 cpplint
+    const cpplintPath = '';
+    await spawn('[installCpplint]', 'python', [`${cpplintPath}/setup.py`, 'install']);
+    return true;
+  } catch (e) { }
+  return false;
+}
+
+export async function installVSCode() {
+  if (isMac) {
+    // 解压 zip
+    // 复制 app 到 /Applications
+    return true;
+  } else if (isWindows) {
+
+    return true;
+  }
+  return false;
+}
+
+export async function installVsix(vsixPath) {
+  try {
+    await spawn('[installVsix]', 'code', ['--install-extension', vsixPath]);
+    return true;
+  } catch (e) { }
+  return false;
+}
--- src/main/utils/child-process.ts
+++ src/main/utils/child-process.ts
@@ -1,23 +1,32 @@
-import { spawn as _spawn, PromisifySpawnOptions, Output } from 'promisify-child-process';
+import { ExecFileOptions } from 'child_process';
+import { spawn as _spawn, execFile as _execFile, PromisifySpawnOptions, Output } from 'promisify-child-process';
 import { logProcess } from 'common/utils/logger';
 
 const SHELL_TIMEOUT = 10 * 1000;
 
-const commonExecOptions = {
+const commonSpawnOptions = {
   encoding: 'utf8',
   timeout: SHELL_TIMEOUT,
   windowsHide: true,
   shell: true,
 };
 
+const commonExecFileOptions = {
+  encoding: 'utf8',
+  timeout: SHELL_TIMEOUT,
+  windowsHide: true,
+};
+
 export type SpawnOptions = PromisifySpawnOptions;
+export type ExecFileOptions = ExecFileOptions;
 export type SpawnOutput = Output;
+export type ExecFileOutput = Output;
 export type ChildProcessOutput = string | Buffer | null | undefined;
 
 export function spawn(type: string, cmd: string, args: string[], extraOptions: SpawnOptions = {}) {
   logProcess.info(type, '[spawn.start]', cmd, args.join(' '));
   return _spawn(cmd, args, {
-    ...commonExecOptions,
+    ...commonSpawnOptions,
     ...extraOptions,
   }).then(r => {
     logProcess.info(
@@ -29,6 +38,31 @@ export function spawn(type: string, cmd: string, args: string[], extraOptions: S
   }).catch(e => {
     logProcess.error(
       type, '[spawn.error]', cmd, args.join(' '),
+      '\nerror:', e,
+      '\ncode:', e.code,
+      '\nsignal:', e.signal,
+      '\nstdout:', e.stdout,
+      '\nstderr:', e.stderr
+    );
+    throw e;
+  });
+}
+
+export function execFile(type: string, file: string, args: string[], extraOptions: ExecFileOptions = {}) {
+  logProcess.info(type, '[execFile.start]', file, args.join(' '));
+  return _execFile(file, args, {
+    ...commonExecFileOptions,
+    ...extraOptions,
+  }).then(r => {
+    logProcess.info(
+      type, '[execFile.done]', file, args.join(' '),
+      '\nstdout:', r.stdout,
+      '\nstderr:', r.stderr
+    );
+    return r;
+  }).catch(e => {
+    logProcess.error(
+      type, '[execFile.error]', file, args.join(' '),
       '\nerror:', e,
       '\ncode:', e.code,
       '\nsignal:', e.signal,
